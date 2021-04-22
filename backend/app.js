954e5a52fc46f5d908f07818081d6b8e4f307609
jayparekh4801 <parekhjay4801@gmail.com>
2021-04-22 12:31:14 +0530

    Modified structure

--- backend/app.js
+++ backend/app.js
@@ -4,3 +4,15 @@ const express = require('express');
 const app = express();
 const user = require('./userDetails');
 const userMessage = require('./messages');
+const port = 8000;
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+app.use(express.urlencoded());
+app.use(express.json({ limit: '100mb' }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json('application/json'));
+app.use(cors({ origin: 'http://localhost:4200', optionsSuccessStatus: 200 }));
+
+mongoose.connect('mongodb://localhost/WhatsAppSender', { useNewUrlParser: true, useUnifiedTopology: true });
+
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LogInComponent } from './Components/log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    SignUpComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
